f8bcecef14a0c23e1b2adf60a5027991916b2970
CanhVeTaiGia <nghia120425@gmail.com>
2025-12-16 14:26:21 +0700

    update

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real estate management',
+    'version': '1.0',
+    'depends':['base'],
+    'category': 'Estate',
+    'author': 'Nghia',
+    'description':"""
+    This is a module for managing real estate data
+        - User
+        - Real estate
+        - ...
+    """,
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': "LGPL-3"
+}
